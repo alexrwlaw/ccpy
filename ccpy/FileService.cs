e0e811f3e824b121d4e8403d719d8d21487b1c54
Alex Law <alex.law@LAVENPARTNERS.COM>
2018-11-10 14:42:55 +0000

    Add project files.

---
+++ ccpy/FileService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ccpy
+{
+    class FileService
+    {
+        public void LoadFiles(BindingSource bindingSource)
+        {
+            string[] files = Directory.GetFiles(@"D:\iTunes Media\Music", "*.*", SearchOption.AllDirectories);
+
+            DataTable dataTable = new DataTable();
+
+            dataTable.Columns.Add("Name", typeof(string));
+            dataTable.Columns.Add("Size", typeof(int));
+            dataTable.Columns.Add("Date", typeof(DateTime));
+
+            foreach (string file in files)
+            {
+                FileInfo fileInfo = new FileInfo(file);
+
+                DataRow dataRow = dataTable.NewRow();
+                dataRow["Name"] = file;
+                dataRow["Size"] = fileInfo.Length;
+                dataRow["Date"] = fileInfo.LastWriteTime;
+                dataTable.Rows.Add(dataRow);
+            }
+
+            bindingSource.DataSource = dataTable;
+        }
+    }
+}
